50b0a784a0a40e101e35348a7ca986753c34e85b
GSosa97 <63225841+Gabisosa97@users.noreply.github.com>
2022-10-26 20:22:35 -0300

    + Cambos esteticos

--- FrontEnd/src/app/components/about-me/about-me.component.ts
+++ FrontEnd/src/app/components/about-me/about-me.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
-  persona: persona = new this.persona({})
+
   constructor() { }
 
   ngOnInit(): void {
