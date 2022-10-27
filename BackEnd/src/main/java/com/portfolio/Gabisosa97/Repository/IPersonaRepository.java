adc787c40715d924e7b251ae7f65dd6678d37c31
GSosa97 <63225841+Gabisosa97@users.noreply.github.com>
2022-10-27 15:18:45 -0300

    + BackEnd agregado

---
+++ BackEnd/src/main/java/com/portfolio/Gabisosa97/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.Gabisosa97.Repository;
+
+import com.portfolio.Gabisosa97.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
---
+++ BackEnd/src/main/java/com/portfolio/Gabisosa97/Security/Entity/Rol.java
@@ -0,0 +1,28 @@
+package com.portfolio.Gabisosa97.Security.Entity;
+
+import com.portfolio.Gabisosa97.Security.Enums.RolNombre;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+
+    //Constructor
+    public Rol() {
+    }
+    
+    
+}
